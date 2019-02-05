53d64d26efa789497b85eaeb4c95c0a6f4371cbb
Chris Billington <Chris Billington@localhost>
2019-02-05 17:32:57 -0500

    Add about.py. This will contain a dialog with information about the labscript suite.

---
+++ about.py
@@ -0,0 +1,58 @@
+ABOUT = """
+Thank you for installing the labscript suite. We hope it is useful!
+
+Just three things before you go:
+
+1. Give credit appropriately
+
+  You can cite the labscript suite as:
+
+  [RSI citation]
+
+  If you use the labscript suite in work that leads to a publication, it is appropriate
+  to cite it. Software is often neglected when it comes to citations, and for commercial
+  software perhaps this is ok since its creators are rewarded financially rather than
+  academically. However the labscript suite is free and was created by academics taking
+  time away from other research, for the benefit of researchers everywhere. Whilst you
+  don't need to cite it in every publication, once per apparatus might be reasonable, or
+  any time you are describing other details of your experimental setup in a published
+  work.
+
+
+2. Ask for help
+
+  We do not yet have comprehensive documentation, and whilst we hope this situation will
+  improve, it is partially mitigated by an active community willing to help. If you're
+  having a problem, search the archive of our mailing list to see if your issue is
+  addressed, and send a message to the mailing list if it isn't. Unless the content of
+  your message includes information you'd rather keep private, it is preferable to post
+  publicly to the mailing list rather than privately emailing one of the developers.
+  This way if multiple people are having the same issue, the solutions will be there for
+  all to see. Feel free to just email the mailing list saying hello. We are always
+  interested to know what the labscript suite is being used for and how we could make it
+  better.
+
+
+3. Contribute back
+
+  The labscript suite is a living software project. If you find a bug, please report it
+  on BitBucket. If you fix a bug, please make a pull request on BitBucket to share the
+  fix. If you implement support for a new device, please make a pull request to get it
+  included. And if you are unsure how to do any of this, don't hesitate to ask on the
+  mailing list. Even if you do not consider your code particularly good quality,
+  contributing it creates a space for anyone interested to discuss what it should look
+  like, rather than several groups implementing the same device code and duplicating
+  effort. As a general rule: if it is running experiments, it is good enough to be
+  included. If there are issues, these can be resolved in the future - merging code into
+  labscript does not freeze it in time (anything but!).
+
+  In particular, If you modify labscript-suite code, but do not contribute it back to
+  mainline, it is likely that future labscript suite updates will break your code. This
+  could leave you unable to update the labscript suite, and limit your changes to only
+  working with that particular apparatus, as it will not be updated to be compatible
+  with newer versions of Python and its libraries, making it difficult to install
+  elsewhere. Getting your changes 'upstreamed' is the best way to ensure they will be
+  taken into account in future updates to the software and prevent your changes from
+  suffering 'bit-rot'.
+
+"""
